660a15974f5ee2411b77a7fc67ba2ab6a0f7caa3
Steve Schmerler <git@elcorto.com>
2023-10-31 15:43:46 +0100

    DOC: add doc string to index.py in autodoctest

--- example_package/autodoctest/src/autodoctest/index.py
+++ example_package/autodoctest/src/autodoctest/index.py
@@ -1,2 +1,9 @@
+"""
+This module is called "index.py" and it defines a function "index()".
+sphinx-autodoc has no problem with this, while sphinx-apidoc will get
+confused, since now there are two files "index.rst", one the index file holding
+the TOC, and the file representing this module.
+"""
+
 def index(foo):
     pass
